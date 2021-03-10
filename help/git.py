4ceb9fdfbb22efb92faba804b38af6c3517932ea
Samuel <themobileprof.com@gmail.com>
2021-03-10 19:48:23 +0100

    git intro

--- help/git.py
+++ help/git.py
@@ -1,1 +1,13 @@
-echo "hello world"
+# Python 3
+# Author: Samuel Anyaele - TheMobileprof.com
+
+print("""This is the TMP Git Helper, what would you like to do?
+1. Add a new Project
+2. Get an existing project from Github
+3. Save my changes
+4. Undo a change
+5. Get the latest version of this project""")
+
+step1 = input("Choose a number 1-5 from the menu above: ")
+
+print("You chose option " + step1)
